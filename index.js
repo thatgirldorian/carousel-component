7e10c4831a2dc8a210124abb96c1fac307c167f5
Debbie Otuagomah <thatgirldorian@gmail.com>
2022-07-18 22:57:53 +0400

    Added sliding functionality

---
+++ index.js
@@ -0,0 +1,6 @@
+//Differentiate between the slider buttons
+
+const carousel = document.querySelector('.carousel')
+
+const previousBtn = carousel.querySelector('.previous-button')
+const nextBtn = carousel.querySelector('.next-button')
